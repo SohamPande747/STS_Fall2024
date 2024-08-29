b142207cbcdb870d5d00d59b5516cd8be32494fe
Soham Pande <sohampande747@gmail.com>
2024-08-29 22:08:56 +0530

    3rd pdf

---
+++ bitOp.java
@@ -0,0 +1,15 @@
+public class bitOp
+{
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = 20;
+
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(~b);
+		System.out.println(a >> 2);
+		System.out.println(a << 2);
+		System.out.println(a >>> 2);
+	}
+}
---
+++ ternOp.java
@@ -0,0 +1,13 @@
+public class ternOp
+{
+	public static void main(String args[])
+	{
+		int a = 4;
+		int b = 9;
+
+		int min = (a>b)?a:b;
+
+		System.out.println(min);
+	}
+}
+
